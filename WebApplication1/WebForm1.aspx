c3d3192f430f6be57eedb522881e200c71b6cc7a
Shawn Chaudhry <shawnchaudhry@hotmail.com>
2016-04-07 15:28:38 -0400

    This is the second assignment from KnackTek

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>Welcome to Your Calculator!</div>
+    Number 1: <asp:TextBox ID="tbOne" runat="server"></asp:TextBox>
+        <br />
+    Number 2: <asp:TextBox ID="tbTwo" runat="server"></asp:TextBox>
+        <asp:RadioButtonList ID="rblOperations" runat="server">
+            <asp:ListItem>Add</asp:ListItem>
+            <asp:ListItem>Subtract</asp:ListItem>
+            <asp:ListItem>Multiply</asp:ListItem>
+            <asp:ListItem>Divide</asp:ListItem>
+        </asp:RadioButtonList>
+        <p>
+            <asp:Button ID="btnCalculate" runat="server" Text="Calculate!" onClick="finish"/>
+        </p>
+        <asp:Label ID="lblAnswer" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public double add(double arg0, double arg1)
+        {
+            double arg2 = arg0 + arg1;
+            return arg2;
+        }
+
+        public double subtract(double arg0, double arg1)
+        {
+            double arg2 = arg0 - arg1;
+            return arg2;
+        }
+        public double multiply(double arg0, double arg1)
+        {
+            double arg2 = arg0 * arg1;
+            return arg2;
+        }
+        public double divide(double arg0, double arg1)
+        {
+            if (arg1 > 0)
+            {
+                double arg2 = arg0 / arg1;
+                return arg2;
+            }
+            else
+            {
+                double arg2 = 0;
+                return arg2;
+            }
+        }
+
+        public void finish(Object Sender, EventArgs e)
+        {
+            String answer = "";
+            Double arg0 = Double.Parse(tbOne.Text);
+            Double arg1 = Double.Parse(tbTwo.Text);
+
+            if (rblOperations.SelectedValue.CompareTo("Add") == 0)
+            {
+               answer = "" + add(arg0, arg1);
+            }
+
+            if (rblOperations.SelectedValue.CompareTo("Subtract") == 0)
+            {
+                answer = "" + subtract(arg0, arg1);
+            }
+
+            if (rblOperations.SelectedValue.CompareTo("Multiply") == 0)
+            {
+                answer = "" + multiply(arg0, arg1);
+            }
+
+            if (rblOperations.SelectedValue.CompareTo("Divide") == 0)
+            {
+                answer = "" + divide(arg0, arg1);
+            }
+
+            Label answerLabel = new Label();
+            answerLabel.Text = answer;
+            Page.Controls.Add(answerLabel);
+        }
+    }
+}
